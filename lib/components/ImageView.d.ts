676278c299b434386f730043d62778d156d07795
Bodo Tasche <bodo@wannawork.de>
2022-02-23 14:23:57 +0100

    Added links to images

--- lib/components/ImageView.d.ts
+++ lib/components/ImageView.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="react" />
 import { ImageStyle, StyleProp, ViewProps, ViewStyle } from 'react-native';
-import { ImageType } from '../../typings';
+import { ImageType, LinkFunction } from '../../typings';
 declare type ImageProps = ViewProps & {
     element: ImageType;
     wrapperStyle?: StyleProp<ViewStyle>;
     style?: StyleProp<ImageStyle>;
+    onLinkPress?: LinkFunction;
 };
 export declare const ImageView: (props: ImageProps) => JSX.Element;
 export {};
--- lib/services.d.ts
+++ lib/services.d.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, TextProps } from 'react-native';
-import { LinkFunction, RichTextContent, RichTextDefaultStyles, RichTextElementType, RichTextStyles, SpanType } from '../typings';
+import { LinkFunction, LinkType, RichTextContent, RichTextDefaultStyles, RichTextElementType, RichTextStyles, SpanType } from '../typings';
+export declare const linkResolver: (link: LinkType) => string;
 export declare const serializerWithStyle: (styles: RichTextStyles, onLinkPress?: LinkFunction | undefined) => (type: RichTextElementType, element: SpanType, text: string, children: React.ComponentElement<TextProps, Text>, index: string) => React.ComponentElement<TextProps, Text> | null;
 export declare const asText: (structuredText: RichTextContent[]) => any;
 export declare function computeStyles(defaultStyle?: RichTextDefaultStyles, styles?: RichTextStyles): RichTextStyles;
