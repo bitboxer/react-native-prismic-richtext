502d3780c6dc2f7ae2eadc951bf03134fc581764
Bodo Tasche <bodo@wannawork.de>
2022-03-04 10:46:49 +0100

    Add possibility to overwrite elements

--- lib/services.d.ts
+++ lib/services.d.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TextProps } from 'react-native';
-import { LinkFunction, LinkType, RichTextContent, RichTextDefaultStyles, RichTextElementType, RichTextStyles, SpanType } from '../typings';
+import { LinkFunction, LinkType, RichTextContent, RichTextDefaultStyles, RichTextElementType, RichTextSerializer, RichTextStyles, SpanType } from '../typings';
 export declare const linkResolver: (link: LinkType) => string;
-export declare const serializerWithStyle: (styles: RichTextStyles, onLinkPress?: LinkFunction | undefined) => (type: RichTextElementType, element: SpanType, text: string, children: React.ComponentElement<TextProps, Text>, index: string) => React.ComponentElement<TextProps, Text> | null;
+export declare const serializerWithStyle: (styles: RichTextStyles, onLinkPress?: LinkFunction | undefined, serializers?: RichTextSerializer | undefined) => (type: RichTextElementType, element: SpanType, text: string, children: React.ComponentElement<TextProps, Text>, index: string) => React.ComponentElement<TextProps, Text> | null;
 export declare const asText: (structuredText: RichTextContent[]) => any;
 export declare function computeStyles(defaultStyle?: RichTextDefaultStyles, styles?: RichTextStyles): RichTextStyles;
